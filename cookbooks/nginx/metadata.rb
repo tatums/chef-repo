9e2fb19fe2e75590df946aff1765a1c6df6bf5d3
tatum+chef <tatum+chef@ashlandstudios.com>
2014-09-20 21:33:46 +0000

    add nginx and apt cookbooks

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,10 @@
+name             'nginx'
+maintainer       'Spacely Sprokets'
+maintainer_email 'tatum+chef@ashlandstudios.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
+
